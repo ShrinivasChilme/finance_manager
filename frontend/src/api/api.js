030b5a1a5ac05b63633634d96f337f7f453f113d
ShrinivasChilme <chilmeshrinivas6@gmail.com>
2026-04-05 15:53:59 +0530

    changed the app url to public url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5012';
+const API_BASE_URL = 'https://finance-manager-os56.onrender.com';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
